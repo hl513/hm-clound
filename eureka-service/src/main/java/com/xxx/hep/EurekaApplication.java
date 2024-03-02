86da2f21798570c4916fff31b9f068a008c9bde3
hepeng <2592620862@qq.com>
2024-03-02 10:26:59 +0800

    Eureka 服务注册 服务发现 负载均衡

---
+++ eureka-service/src/main/java/com/xxx/hep/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.xxx.hep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author hep
+ * @date 2024/3/2 10:12
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
--- order-service/src/main/java/com/xxx/hep/service/OrderService.java
+++ order-service/src/main/java/com/xxx/hep/service/OrderService.java
@@ -15,12 +15,34 @@ public class OrderService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public Order queryOrderById(Long orderId) {
+    /**
+     *RestTemplate 远程调用 使用 url地址
+     */
+/*    public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
         // 2、利用 RestTemplate 发起 http请求，完成远程调用  查询用户
         // 2.1 url路径
         String url = "http://localhost:8081/user/"+order.getUserId();
+        // 2.2 发送请求
+        User user = restTemplate.getForObject(url, User.class);
+        // 3、 封装User到 order
+        order.setUser(user);
+        // 4.返回
+        return order;
+    }*/
+
+    /**
+     * 远程调用服务  使用 eureka提供的服务名
+     * @param orderId
+     * @return
+     */
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        // 2、利用 RestTemplate 发起 http请求，完成远程调用  查询用户
+        // 2.1 url路径
+        String url = "http://userservice/user/"+order.getUserId();
         // 2.2 发送请求
         User user = restTemplate.getForObject(url, User.class);
         // 3、 封装User到 order
